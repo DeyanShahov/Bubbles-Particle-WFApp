823417d50632255cd45c74ae80bbb120bad7a481
redfoxbg <redfoxbg@gmail.com>
2023-09-10 22:00:51 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Bubbles_Particle_WFApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParticlesTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
